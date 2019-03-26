21267b4c367587b76f2af9bee82590e150341383
Mike Marcacci <mike.marcacci@gmail.com>
2019-03-26 09:16:48 -0700

    actually copy types

---
+++ packages/authx-interface/@types/graphql-api-koa/index.d.ts
@@ -0,0 +1,20 @@
+import { GraphQLSchema } from "graphql";
+import { Middleware, ParameterizedContext } from "koa";
+
+interface ExecuteOptions {
+  schema?: GraphQLSchema;
+  rootValue?: any;
+  contextValue?: any;
+  fieldResolver?: any;
+}
+
+export const errorHandler: <StateT = any, CustomT = {}>() => Middleware<
+  StateT,
+  CustomT
+>;
+
+export const execute: <StateT = any, CustomT = {}>(
+  options: ExecuteOptions & {
+    override?: (ctx: ParameterizedContext<StateT, CustomT>) => ExecuteOptions;
+  }
+) => Middleware<StateT, CustomT>;
