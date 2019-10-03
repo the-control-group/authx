ca0e9acc27415bcbba84298a219baa635d378db3
Mike Marcacci <mike.marcacci@gmail.com>
2019-10-03 10:58:35 -0700

    update x

--- packages/authx/src/x.ts
+++ packages/authx/src/x.ts
@@ -1,6 +1,6 @@
 // Hacked around lack of symbol indexers, see:
 // https://github.com/Microsoft/TypeScript/issues/1863
 //
-// const x: unique symbol = Symbol("AuthX");
-export const x: "x" = Symbol("AuthX") as any;
+// const x: unique symbol = Symbol("@authx/authx");
+export const x: "x" = Symbol("@authx/authx") as any;
 export default x;
