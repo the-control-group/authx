e255124a333e1fc0c521d39aac54e4b1d3cc0d41
Mike Marcacci <mike.marcacci@gmail.com>
2019-10-21 14:00:44 -0700

    Add Authorization.access and .token

--- packages/authx/src/graphql/GraphQLAuthorization.ts
+++ packages/authx/src/graphql/GraphQLAuthorization.ts
@@ -7,10 +7,12 @@ import {
   GraphQLObjectType
 } from "graphql";
 
+import jwt from "jsonwebtoken";
 import { Grant, Authorization, User } from "../model";
 import { Context } from "../Context";
 import { GraphQLGrant } from "./GraphQLGrant";
 import { GraphQLUser } from "./GraphQLUser";
+import { GraphQLTokenFormat } from "./GraphQLTokenFormat";
 
 export const GraphQLAuthorization: GraphQLObjectType<
   Authorization,
@@ -88,6 +90,88 @@ export const GraphQLAuthorization: GraphQLObjectType<
             (await authorization.isAccessibleBy(realm, a, tx, "read.scopes"))
             ? authorization.scopes
             : null;
+        } finally {
+          tx.release();
+        }
+      }
+    },
+    access: {
+      type: GraphQLString,
+      async resolve(
+        authorization,
+        args,
+        { realm, authorization: a, pool }: Context
+      ): Promise<null | string[]> {
+        const tx = await pool.connect();
+        try {
+          return a &&
+            (await authorization.isAccessibleBy(realm, a, tx, "read.scopes"))
+            ? authorization.access(tx)
+            : null;
+        } finally {
+          tx.release();
+        }
+      }
+    },
+    token: {
+      type: GraphQLString,
+      args: {
+        format: {
+          type: new GraphQLNonNull(GraphQLTokenFormat)
+        }
+      },
+      async resolve(
+        authorization,
+        args,
+        {
+          realm,
+          jwtValidityDuration,
+          privateKey,
+          authorization: a,
+          pool
+        }: Context
+      ): Promise<null | string> {
+        const tx = await pool.connect();
+        if (!a) return null;
+        try {
+          if (
+            !(await authorization.isAccessibleBy(
+              realm,
+              a,
+              tx,
+              "read.scopes"
+            )) ||
+            !(await authorization.isAccessibleBy(realm, a, tx, "read.secrets"))
+          ) {
+            return null;
+          }
+
+          if (args.format === "basic") {
+            return `Basic ${Buffer.from(
+              `${authorization.id}:${authorization.secret}`,
+              "utf8"
+            ).toString("base64")}`;
+          }
+
+          if (args.format === "bearer") {
+            const grant = await authorization.grant(tx);
+            return `Bearer ${jwt.sign(
+              {
+                aid: authorization.id,
+                scopes: await authorization.access(tx)
+              },
+              privateKey,
+              {
+                algorithm: "RS512",
+                expiresIn: jwtValidityDuration,
+                audience: (grant && grant.clientId) || undefined,
+                subject: authorization.userId,
+                issuer: realm
+              }
+            )}`;
+          }
+
+          throw new Error("INVARIANT: Impossible token format.");
         } finally {
           tx.release();
         }
