2f1ad4484dbe898135124f17d3aeee2cf5c65b2b
Mike Marcacci <mike.marcacci@gmail.com>
2020-01-14 16:54:08 -0800

    fix formatting

--- packages/tools/src/lib/schema.ts
+++ packages/tools/src/lib/schema.ts
@@ -2,9 +2,9 @@ import { ClientBase, Client } from "pg";
 import fs from "fs";
 
 const sql = fs
-	.readFileSync(require.resolve("@authx/authx/schema.sql"))
-	.toString("utf8");
+  .readFileSync(require.resolve("@authx/authx/schema.sql"))
+  .toString("utf8");
 
 export async function schema(tx: ClientBase | Client): Promise<void> {
-	await tx.query(sql);
+  await tx.query(sql);
 }
