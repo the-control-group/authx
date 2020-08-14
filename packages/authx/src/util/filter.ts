2392e4d7c178a79a3338d51f0eff913841b4121d
Mike Marcacci <mike.marcacci@gmail.com>
2020-08-14 02:21:56 -0700

    fix styles

--- packages/authx/src/util/filter.ts
+++ packages/authx/src/util/filter.ts
@@ -1,18 +1,18 @@
 const EMPTY: unique symbol = Symbol("empty");
 export async function filter<T>(
-	iter: Iterable<T>,
-	callback: (item: T, index: number) => boolean | Promise<boolean>
+  iter: Iterable<T>,
+  callback: (item: T, index: number) => boolean | Promise<boolean>
 ): Promise<T[]> {
-	const array = [...iter];
+  const array = [...iter];
 
-	// Use our unique symbol to distinguish between an intentionally "undefined"
-	// value and a truly omitted one.
-	const result: (T | typeof EMPTY)[] = new Array(array.length).fill(EMPTY);
-	await Promise.all(
-		array.map(async (item: T, index: number) => {
-			if (await callback(item, index)) result[index] = item;
-		})
-	);
+  // Use our unique symbol to distinguish between an intentionally "undefined"
+  // value and a truly omitted one.
+  const result: (T | typeof EMPTY)[] = new Array(array.length).fill(EMPTY);
+  await Promise.all(
+    array.map(async (item: T, index: number) => {
+      if (await callback(item, index)) result[index] = item;
+    })
+  );
 
-	return result.filter((r): r is T => r !== EMPTY);
+  return result.filter((r): r is T => r !== EMPTY);
 }
