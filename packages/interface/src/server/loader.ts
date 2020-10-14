846d2af02b71dfdac0fb6ad25f7110f114be715f
Mike Marcacci <mike.marcacci@gmail.com>
2020-10-14 14:53:50 -0700

    add missing type

--- packages/interface/src/server/loader.ts
+++ packages/interface/src/server/loader.ts
@@ -1,9 +1,9 @@
-import { loader } from "webpack";
+// The LoaderContext type is no longer published, which is unfortunate.
+type LoaderContext = {
+  query?: { strategies?: string[] };
+};
 
-export default function (
-  this: loader.LoaderContext,
-  source: string | Buffer
-): string {
+export default function (this: LoaderContext, source: string | Buffer): string {
   const strategies: ReadonlyArray<string> =
     (this.query && this.query.strategies) || [];
   source = typeof source === "string" ? source : source.toString();
