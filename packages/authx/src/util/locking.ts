086581ce031f758a79d4f8749c03af09affa3299
Eric Brown <eric.brown@peopleconnect.us>
2021-02-04 15:10:56 -0800

    Extra check added

--- packages/authx/src/util/locking.ts
+++ packages/authx/src/util/locking.ts
@@ -42,6 +42,22 @@ async function lockEntityType(
   entityName: string,
   ids: readonly string[]
 ): Promise<void> {
+  // Our Postgres client doesn't allow escaping of table names, so be extra careful
+  // just in case some code allows through untrusted data.
+  if (
+    ![
+      "authority",
+      "authorization",
+      "client",
+      "credential",
+      "grant",
+      "role",
+      "user",
+    ].includes(entityName)
+  ) {
+    throw Error(`${entityName} is not a recognized entity type`);
+  }
+
   await tx.query(
     `SELECT id FROM authx.${entityName} WHERE id = ANY($1) FOR UPDATE`,
     [ids]
